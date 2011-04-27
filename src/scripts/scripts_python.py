8a528bf4dbd49db0f1879d4060c2e68be861118a
whanderley <whanderley.souza@gmail.com>
2011-04-27 17:02:01 -0300

    create revise two points

--- src/scripts/scripts_python.py
+++ src/scripts/scripts_python.py
@@ -1,13 +1,9 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import rlcompleter
 try:
     import vim
-    cb = vim.current.buffer
 except ImportError:
     # Isn't in vim. Probably this is a test.
     pass
@@ -24,22 +20,31 @@ def revise_imports(line, i):
     words = [match.group() for match in re.finditer("[A-Za-z_]+", line)]
     if words:
         if (words[0] == "import") and (len(words) > 2):
-            cb[i] = "import " + words[1]
+            vim.current.buffer[i] = "import " + words[1]
             for ind, imp in enumerate(words[2:]):
-                cb.append("import " + imp, i + ind + 1)
+                vim.current.buffer.append("import " + imp, i + ind + 1)
 
 def revise_header():
-    if cb[0] != "#!/usr/bin/env python":
-        cb.append("#!/usr/bin/env python", 0)
-    if cb[1] != "# -*- coding: utf-8 -*-":
-        cb.append("# -*- coding: utf-8 -*-", 1)
-        cb.append("")
+    if vim.current.buffer[0] != "#!/usr/bin/env python":
+        vim.current.buffer.append("#!/usr/bin/env python", 0)
+    if vim.current.buffer[1] != "# -*- coding: utf-8 -*-":
+        vim.current.buffer.append("# -*- coding: utf-8 -*-", 1)
+        vim.current.buffer.append("")
 
-def revise_name_class(line, i):
+def revise_name_class(line):
     words = [match.group() for match in re.finditer("[A-Za-z_]+", line)]
     if words:
-        if words[0] == "class":
-            cb[i] = line.replace(words[1],  words[1].capitalize())
+        if words[0] == "class" and len(words)>1:
+            name_class = words[1][0].capitalize() + words[1][1:]
+            return line.replace(words[1], name_class)
+    return line
+
+def revise_two_points(line):
+    while " :" in line:
+        line = line.replace(" :", ":")
+    while ":  " in line:
+        line = line.replace(":  ", ": ")
+    return line
 
 def revise_parenthesis(line):
     while "( " in line:
@@ -50,9 +55,10 @@ def revise_parenthesis(line):
 
 def parse2pep08():
     revise_header()
-    for i,line in enumerate(cb):
-        revise_name_class(line, i)
+    for i,line in enumerate(vim.current.buffer):
         revise_imports(line, i)
+        vim.current.buffer[i] = revise_two_points(line)
+        vim.current.buffer[i] = revise_name_class(line)
         vim.current.buffer[i] = revise_parenthesis(line)
 
 def insert_header():
@@ -61,19 +67,20 @@ def insert_header():
     vim.current.buffer.append("")
 
 def create_imports_for_tests():
-    full_filename = cb.name
+    full_filename = vim.current.buffer.name
     filename = full_filename.split("/")[-1]
     name = get_program_name(filename)
-    if filename.find("spec") != -1 and cb[2] == "" and len(cb) < 5:
-        cb[len(cb)-1] = "import unittest"
-        cb.append("from should_dsl import should")
-        cb.append("from " + name + " import ")
-        cb.append("")
-        cb.append("class Test" + name.capitalize() + "(unittest.TestCase):")
+    if filename.find("spec") != -1 and vim.current.buffer[2] == "" and len(vim.current.buffer) < 5:
+        vim.current.buffer[len(vim.current.buffer)-1] = "import unittest"
+        vim.current.buffer.append("from should_dsl import should")
+        vim.current.buffer.append("from " + name + " import ")
+        vim.current.buffer.append("")
+        vim.current.buffer.append("class Test" + name.capitalize() + "(unittest.TestCase):")
         vim.command(":w")
-        vim.current.window.cursor = (6, len(cb[-1]))
+        vim.current.window.cursor = (6, len(vim.current.buffer[-1]))
         vim.command("tabnew " + name + ".py")
         insert_header()
         vim.command(":w")
         vim.command("tabp")
+
 
--- src/scripts/spec_python.py
+++ src/scripts/spec_python.py
@@ -1,15 +1,29 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import unittest
 from should_dsl import should
-from scripts_python import revise_parenthesis
+from scripts_python import revise_parenthesis, revise_name_class, revise_two_points
 
 
-class TestPython(unittest.TestCase):
+class Testpython(unittest.TestCase):
 
     def test_remove_spaces_after_parenthesis(self):
         revise_parenthesis("  (  a)") |should| equal_to("  (a)")
 
     def test_remove_spaces_before_parenthesis(self):
         revise_parenthesis("  (a  )") |should| equal_to("  (a)")
+
+    def test_capitalize_clasname_in_line_with_class_definition(self):
+        revise_name_class("class foo():") |should| equal_to("class Foo():")
+        revise_name_class("a = b + 1") |should| equal_to("a = b + 1")
+        revise_name_class("class Foo():") |should| equal_to("class Foo():")
+        revise_name_class("class FooFoo():") |should| equal_to("class FooFoo():")
+
+    def test_remove_spaces_after_two_points(self):
+        revise_two_points("def foo()   :") |should| equal_to("def foo():")
+        revise_two_points("def foo():") |should| equal_to("def foo():")
+
+    def test_remove_spaces_before_two_points(self):
+        revise_two_points("def foo():     ") |should| equal_to("def foo(): ")
+        revise_two_points("def foo(): print") |should| equal_to("def foo(): print")
